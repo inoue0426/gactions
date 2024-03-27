51dbd766b79680832ab3db1f23df817a5f5af0c4
inoue0426 <sekihan_tennis@yahoo.co.jp>
2024-03-27 10:42:45 -0400

    test

---
+++ test.py
@@ -0,0 +1,36 @@
+import pandas as pd 
+import numpy as np 
+
+def test():
+    
+    print("Hello World")
+    
+    return 1
+
+
+def test2():
+    
+    print("Hello World 2")
+    
+    
+    return 2
+
+class test_func():
+    
+    def __init__(self):
+        print("Hello World 3")
+        
+    def test3(self):
+        print("Hello World 3")
+         
+        return 3
+
+class test_func2():
+    
+    def __init__(self):
+        print("Hello World 4")
+        
+    def test4(self):
+        print("Hello World 4")
+        
+        return 4
