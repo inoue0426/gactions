c0dc0d0c885acf86670303236ad50af549a86ccc
inoue0426 <inoue0426@users.noreply.github.com>
2024-03-27 15:06:29 +0000

    Apply auto-formatting

--- test/test2.py
+++ test/test2.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 
+
 def split_date():
 
     # Read data
@@ -15,12 +16,8 @@ def split_date():
     data = pd.read_csv("src/Helsingin_pyorailijamaarat.csv", sep=";")
 
     data = data.dropna(how="all")
-    
-    
+
     data = data.dropna(axis=1)
-
-
-
 
     # Split the first column
     data = data["Päivämäärä"].str.split(expand=True)
@@ -29,16 +26,41 @@ def split_date():
     data.columns = ["Weekday", "Day", "Month", "Year", "Hour"]
 
     # Finnish to English
-    data["Weekday"] = data["Weekday"].map({"ma": "Mon", "ti": "Tue", "ke": "Wed", "to": "Thu", "pe": "Fri", "la": "Sat", "su": "Sun"})
+    data["Weekday"] = data["Weekday"].map(
+        {
+            "ma": "Mon",
+            "ti": "Tue",
+            "ke": "Wed",
+            "to": "Thu",
+            "pe": "Fri",
+            "la": "Sat",
+            "su": "Sun",
+        }
+    )
 
     # Finnish to English
-    data["Month"] = data["Month"].map({"tammi": 1, "helmi": 2, "maalis": 3, "huhti": 4, "touko": 5, "kesä": 6, "heinä": 7, "elo": 8, "syys": 9, "loka": 10, "marras": 11, "joulu": 12})
+    data["Month"] = data["Month"].map(
+        {
+            "tammi": 1,
+            "helmi": 2,
+            "maalis": 3,
+            "huhti": 4,
+            "touko": 5,
+            "kesä": 6,
+            "heinä": 7,
+            "elo": 8,
+            "syys": 9,
+            "loka": 10,
+            "marras": 11,
+            "joulu": 12,
+        }
+    )
 
     # Convert to integers
     data["Day"] = data["Day"].map(int)
     data["Year"] = data["Year"].map(int)
-    
+
     # Convert to integers
-    data["Hour"] = data["Hour"].str.split(":", expand=True)[0].map(int) 
+    data["Hour"] = data["Hour"].str.split(":", expand=True)[0].map(int)
 
     return data
