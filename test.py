195c1267862e10780a7ec3b8b82675b8fd011010
inoue0426 <inoue0426@users.noreply.github.com>
2024-03-27 14:49:56 +0000

    Apply auto-formatting

--- test.py
+++ test.py
@@ -1,36 +1,38 @@
-import pandas as pd 
-import numpy as np 
+import pandas as pd
+import numpy as np
+
 
 def test():
-    
+
     print("Hello World")
-    
+
     return 1
 
 
 def test2():
-    
+
     print("Hello World 2")
-    
-    
+
     return 2
 
-class test_func():
-    
+
+class test_func:
+
     def __init__(self):
         print("Hello World 3")
-        
+
     def test3(self):
         print("Hello World 3")
-         
+
         return 3
 
-class test_func2():
-    
+
+class test_func2:
+
     def __init__(self):
         print("Hello World 4")
-        
+
     def test4(self):
         print("Hello World 4")
-        
+
         return 4
